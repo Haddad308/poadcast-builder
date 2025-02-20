8ad5256cd217aab457b62aab8a38d4680485e54d
Haddad308 <melhaddad308@gmail.com>
2025-02-20 04:26:16 +0200

    fix: update import path for dynamic page loading

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@
 
 import dynamic from "next/dynamic";
 
-const Page = dynamic(() => import("../app/index"), { ssr: false });
+const Page = dynamic(() => import("./index"), { ssr: false });
 
 export default Page;
