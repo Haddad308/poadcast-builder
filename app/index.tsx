8d7f91843551bf99560cd9f1a1556640557231e4
Haddad308 <melhaddad308@gmail.com>
2025-04-06 09:56:38 +0200

    fix: redirect unauthenticated users to the signin page

--- app/index.tsx
+++ app/index.tsx
@@ -45,8 +45,8 @@ const Page = () => {
   const router = useRouter();
 
   useEffect(() => {
-    if (user) {
-      router.push("/");
+    if (!user) {
+      router.push("/signin");
     }
   }, [user, router]);
 
